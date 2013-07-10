2472d595c6172f03323c0580064b7c225f1d7d44
Adam Brenecki <adam@brenecki.id.au>
2013-07-10 13:00:25 +0930

    Add default imports, admin, urls to djangoapp

---
+++ djangoapp/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from .views import *
+
+urlpatterns = patterns('',
+    # Example:
+    # url(r'^$', my_view, name='home'),
+    # url(r'^stuff/$', MyClassView.as_view(), name='stuff'),
+)
